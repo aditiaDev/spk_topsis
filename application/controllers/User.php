20cd1bf9986b9dd47fa8bf65e18dc67c7b7e66b0
aditiaDev <urbanaditia@gmail.com>
2023-06-27 20:36:00 +0700

    Create master data

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class User extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
@@ -14,7 +14,7 @@ class Home extends CI_Controller {
 	{
 		$this->load->view('template/header');
     $this->load->view('template/topmenu');
-    $this->load->view('pages/home');
+    $this->load->view('pages/user');
     $this->load->view('template/footer');
 	}
 }
--- application/views/template/topmenu.php
+++ application/views/template/topmenu.php
@@ -2,33 +2,21 @@
 <nav class="top-nav">
     <ul>
         <li>
-            <a href="<?php echo base_url("home")?>" class="top-menu top-menu--active">
+            <a href="<?php echo base_url("home")?>" class="top-menu <?= ($this->uri->segment(1) == 'home') ? 'top-menu--active' : '' ?>">
                 <div class="top-menu__icon"> <i data-feather="home"></i> </div>
                 <div class="top-menu__title"> Home  </div>
             </a>
         </li>
         <li>
-            <a href="javascript:;" class="top-menu">
+            <a href="javascript:;" class="top-menu <?= ($this->uri->segment(1) == 'master') ? 'top-menu--active' : '' ?>">
                 <div class="top-menu__icon"> <i data-feather="box"></i> </div>
-                <div class="top-menu__title"> Menu Layout <i data-feather="chevron-down" class="top-menu__sub-icon"></i> </div>
+                <div class="top-menu__title"> Master Data  <i data-feather="chevron-down" class="top-menu__sub-icon"></i> </div>
             </a>
             <ul class="">
                 <li>
-                    <a href="index.html" class="top-menu">
-                        <div class="top-menu__icon"> <i data-feather="activity"></i> </div>
-                        <div class="top-menu__title"> Side Menu </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="simple-menu-light-dashboard-overview-1.html" class="top-menu">
-                        <div class="top-menu__icon"> <i data-feather="activity"></i> </div>
-                        <div class="top-menu__title"> Simple Menu </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="top-menu-light-dashboard-overview-1.html" class="top-menu">
-                        <div class="top-menu__icon"> <i data-feather="activity"></i> </div>
-                        <div class="top-menu__title"> Top Menu </div>
+                    <a href="<?php echo base_url("master/user")?>" class="top-menu">
+                        <div class="top-menu__icon"> <i data-feather="users"></i> </div>
+                        <div class="top-menu__title"> Data User </div>
                     </a>
                 </li>
             </ul>
